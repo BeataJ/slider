0c1b00f3a9a8b05c486ef3391b0666a1e2cdccaa
beata <beata@emc2wave.com>
2020-08-14 22:40:02 -0700

    add prevBtn display none

--- app.js
+++ app.js
@@ -19,14 +19,17 @@ prevBtn.addEventListener('click', () => {
 
 const carousel = () => {
   // working with slides
-  if (counter === slides.length) {
-    counter = 0;
-  }
-  if (counter < 0) {
-    counter = slides.length - 1;
-  }
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+
+  // working with buttons
 
   slides.forEach((slide) => {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 };
+prevBtn.style.display = 'none';
