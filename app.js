8b9294eaf7cbc87083c3478ed18dce7b1456064d
beata <beata@emc2wave.com>
2020-08-14 22:34:36 -0700

    add if counter === slides.length

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ prevBtn.addEventListener('click', () => {
 });
 
 const carousel = () => {
+  // working with slides
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
   slides.forEach((slide) => {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
